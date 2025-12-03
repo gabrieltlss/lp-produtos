e66d3fbcd66bb6485dea9d087342326a8e55974b
Gabriel Teles <bilebile2020@gmail.com>
2025-12-03 16:42:41 -0300

    Cria rota para renderizar view de login; Modifica package.json para adicionar script de teste.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 const { Router } = require("express");
+const { obterUsuario } = require("../repository/repositorioUsuario");
 
 const roteador = Router();
 
-roteador.get("/", (req, res) => {
-    res.render("test");
+// Rotas do administrador
+roteador.get("/admin", async (req, res) => {
+    try {
+        res.render("login");
+    } catch (error) {
+        res.status(404).json({ error: "Erro ao renderizar visão." });
+    }
 });
 
 module.exports = roteador;
