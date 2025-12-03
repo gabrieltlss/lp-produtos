74090d4274c97c9e702a2b367acbf5f2c6d21fe7
Gabriel Teles <bilebile2020@gmail.com>
2025-12-03 15:46:15 -0300

    Modifica repositorio do usuário. Remove try/catch.

--- repository/repositorioUsuario.js
+++ repository/repositorioUsuario.js
@@ -2,28 +2,20 @@ const { client, pool } = require("../database/bancoDeDados");
 
 // Funções para usuário administrados
 async function obterUsuario(email) {
-    try {
-        const [usuario] = await pool.execute(
-            "SELECT * FROM usuario WHERE email = ?;",
-            [email]
-        );
-        return usuario[0];
-    } catch (error) {
-        throw new Error("Erro ao obter usuário");
-    }
+    const [usuario] = await pool.execute(
+        "SELECT * FROM usuario WHERE email = ?;",
+        [email]
+    );
+    return usuario[0];
 }
 
 async function criarUsuario(email, senha) {
-    try {
-        // MySQL não possui função 'returning'. Pensa noutra solução.
-        const [resposta] = await pool.execute(
-            "INSERT INTO usuario (email, senha) VALUES (?, ?);",
-            [email, senha]
-        );
-        return resposta;
-    } catch (error) {
-        throw new Error("Erro ao criar usuário");
-    }
+    // MySQL não possui função 'returning'. Pensa noutra solução.
+    const [resposta] = await pool.execute(
+        "INSERT INTO usuario (email, senha) VALUES (?, ?);",
+        [email, senha]
+    );
+    return resposta;
 }
 
 async function excluirUsuario(email) {
