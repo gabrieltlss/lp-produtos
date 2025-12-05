c9dba2e6cb8dbeab3f066265aac1f70c6cda883f
Gabriel Teles <bilebile2020@gmail.com>
2025-12-05 16:31:21 -0300

    Muda estrutura de pastas; move-as para a pasta src.

--- middlewares/autenticarUsuario.js
+++
@@ -1,9 +0,0 @@
-function autenticarUsuario(req, res, next) {
-    if (req.session.user) {
-        next();
-    } else {
-        res.redirect("/admin");
-    }
-}
-
-module.exports = { autenticarUsuario };
