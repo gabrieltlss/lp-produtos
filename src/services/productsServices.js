4343ea9fa7743bb9c860d8a3777b17af71a2c6a5
Gabriel Teles <bilebile2020@gmail.com>
2025-12-26 11:15:06 -0300

    Define serviços para produtos.

--- src/services/productsServices.js
+++ src/services/productsServices.js
@@ -1,11 +1,28 @@
-const { getProductsDb, createProductDb } = require("../repository/productRepository.js");
-const { generateProductId } = require("./validationServices.js");
+const { getAllProductsDb, createProductDb, updateProductDb, deleteProductDb } = require("../repository/productRepository.js");
 
-async function createProduct(name, price, url, imgPath) {
-    const prodId = generateProductId();
-    const [product] = await createProductDb(prodId, name, price, url, imgPath);
-    if (!product.affectedRows) return { valid: false, error: "Produto não criado." };
+async function getAllProducts() {
+    const [rows] = await getAllProductsDb();
+    if (rows.length === 0) return { valid: false, error: "Não há produtos criados." };
+    return { valid: true, res: rows };
+}
+
+async function createProduct(name, price, url, imgPath, categoryId) {
+    const [rows] = await createProductDb(name, price, url, imgPath, categoryId);
+    if (!rows.affectedRows) return { valid: false, error: "Produto não criado." };
     return { valid: true };
 }
 
-module.exports = { createProduct };
+async function updateProduct(productObject) {
+    const { name, price, url, imgPath, categoryId } = productObject;
+    const [rows] = await updateProductDb(name, price, url, imgPath, categoryId);
+    if (!rows.affectedRows) return { valid: false, error: "Produto não atualizado." };
+    return { valid: true };
+}
+
+async function deleteProduct(productId) {
+    const [rows] = await deleteProductDb(productId);
+    if (!rows.affectedRows) return { valid: false, error: "Produto não excluído." };
+    return { valid: true };
+}
+
+module.exports = { getAllProducts, createProduct, updateProduct, deleteProduct };
