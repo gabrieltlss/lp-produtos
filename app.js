11ad690bcb059edc8115350149cd037f2b367b54
Gabriel Teles <bilebile2020@gmail.com>
2026-01-11 10:35:39 -0300

    Modifica app.js e cookies.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set("views", path.join(__dirname, "src", "views"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set("trust proxy", 1);
 app.use(session({
     secret: process.env.SECRET_SESSION,
     resave: false,
